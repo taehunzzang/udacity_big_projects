155118036b11c8cd461e8997cac49275bb9468da
taehun333 <taehun333@gmail.cim>
2015-12-29 07:55:46 +0900

    Add App Engine

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +13,14 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.MyJokes;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.taehun.jokesactivity.JokesActivity;
+import com.taehun.myapplication.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,16 +54,58 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void tellJoke(View view){
-//        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-//    }
+
     public void tellJoke(View view) {
-        Intent intent = new Intent(this,JokesActivity.class);
-        MyJokes mMyJokes = new MyJokes();
-        String joke = mMyJokes.getJokes();
-        intent.putExtra(JokesActivity.JOKE_KEY,joke);
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this,"Taehun"));
+//        Intent intent = new Intent(this,JokesActivity.class);
+//        MyJokes mMyJokes = new MyJokes();
+//        String joke = mMyJokes.getJokes();
+//        intent.putExtra(JokesActivity.JOKE_KEY,joke);
+//        startActivity(intent);
 //        Toast.makeText(this, mMyJokes.getJokes(), Toast.LENGTH_SHORT).show();
+    }
+
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private  MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null){
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),new AndroidJsonFactory(), null)
+//                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setRootUrl("https://build-it-bigger-1173.appspot.com/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try{
+                return myApiService.sayHi(name).execute().getData();
+            }catch (IOException e){
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            Toast.makeText(context,result,Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(getApplicationContext(),JokesActivity.class);
+//            MyJokes mMyJokes = new MyJokes();
+//            String joke = mMyJokes.getJokes();
+            intent.putExtra(JokesActivity.JOKE_KEY,result);
+            startActivity(intent);
+        }
     }
 
 }
